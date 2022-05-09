f47ce417dbf51c1eec18c3b70cbe5e87f0269ea2
BlasterAlex <pichulev23071999@gmail.com>
2022-05-09 11:23:21 +0400

    Update install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -7,7 +7,7 @@ cmd /c "cd .. & git pull & git submodule update --init --recursive"
 cmd /c "cd ../pytube & git checkout master"
 
 @REM Create pytube binary fail
-cmd /c "cd ../pytube/scripts & create_wheel"
+cmd /c "cd ../pytube/scripts & pip install wheel & create_wheel"
 
 @REM Install all requirements
 cmd /c "cd .. & pip install -r requirements.txt --force-reinstall"
