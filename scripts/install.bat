8bd8804dc5783e62dbdab21ba17965900d293ea1
BlasterAlex <pichulev23071999@gmail.com>
2022-05-09 12:56:19 +0400

    Fix grammatical

--- scripts/install.bat
+++ scripts/install.bat
@@ -6,8 +6,11 @@ cmd /c "cd .. & git pull & git submodule update --init --recursive"
 @REM Checkout pytube master
 cmd /c "cd ../pytube & git checkout master"
 
-@REM Create pytube binary fail
+@REM Create pytube binary file
 cmd /c "cd ../pytube/scripts & pip install wheel & create_wheel"
 
 @REM Install all requirements
 cmd /c "cd .. & pip install -r requirements.txt --force-reinstall"
+
+@REM Waiting for a user keypress
+pause
