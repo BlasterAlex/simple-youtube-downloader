1acdfd470dcbbc462542cfabd70086f4d6f33b91
BlasterAlex <pichulev23071999@gmail.com>
2022-05-09 11:17:56 +0400

    Change install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,3 +1,13 @@
 @echo off
 
+@REM Update project and submodules
+cmd /c "cd .. & git pull & git submodule update --init --recursive"
+
+@REM Checkout pytube master
+cmd /c "cd ../pytube & git checkout master"
+
+@REM Create pytube binary fail
+cmd /c "cd ../pytube/scripts & create_wheel"
+
+@REM Install all requirements
 cmd /c "cd .. & pip install -r requirements.txt --force-reinstall"
