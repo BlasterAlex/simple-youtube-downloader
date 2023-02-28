a291648375263f66348fbe626add857954e3901e
BlasterAlex <pichulev23071999@gmail.com>
2023-02-28 15:38:28 +0400

    Improve install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,16 +1,23 @@
 @echo off
 
+@REM Get project dir
+for %%a in (%~dp0\.) do set projectDir=%%~dpa
+
 @REM Update project and submodules
-cmd /c "cd .. & git pull & git submodule update --init --recursive"
+cmd /c "cd %projectDir% & git pull & git submodule update --init --recursive" || goto :end
 
 @REM Checkout pytube master
-cmd /c "cd ../pytube & git checkout master"
+cmd /c "cd %projectDir%/pytube & git checkout master" || goto :end
 
 @REM Create pytube binary file
-cmd /c "cd ../pytube/scripts & pip install wheel & create_wheel"
+cmd /c "cd %projectDir%/pytube/scripts & pip install wheel & create_wheel" || goto :end
 
 @REM Install all requirements
-cmd /c "cd .. & pip install -r requirements.txt --force-reinstall"
+cmd /c "cd %projectDir% & pip install -r requirements.txt --force-reinstall" || goto :end
+
+@REM Do not track configuration file changes
+cmd /c "cd %projectDir% & git update-index --assume-unchanged config/settings.yml" || goto :end
 
 @REM Waiting for a user keypress
+:end
 pause
